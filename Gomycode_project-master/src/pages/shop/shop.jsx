e53bb05ff084a6fdb6f028ad6daf67665fe6c56f
lawalemma24 <lawalemma24@gmail.com>
2025-09-03 11:01:59 +0100

    project setup

---
+++ Gomycode_project-master/src/pages/shop/shop.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./shop.css";
+import { Navbar } from "../../components/navbar";
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <Navbar/>
+      <div className="shopTitle">
+        <h1>DIGITAL SHOPPING PLATFORM</h1>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
