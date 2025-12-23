c4c3c51af6de26ef6168fab17d990b18abfc6d6d
lawalemma24 <lawalemma24@gmail.com>
2025-12-23 08:00:14 +0100

    push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,5 @@
 import React from "react"
-// import HomePage from "./pages/HomePage.jsx"
-// import NoteDetail from "./pages/NoteDetail.jsx"
 import CreateNote from "./pages/CreateNote.jsx"
-// import { Routes, Route, Navigate } from "react-router-dom"
-// import { toast } from "react-hot-toast"
-// import { Route } from "react-router-dom"
-// import HomePage from "./pages/HomePage.jsx"
 import './app.css'
 // import UpdateNote from "./pages/UpdateNote.jsx"
 import Dashboard from "./pages/Dashboard.jsx"
@@ -23,8 +17,7 @@ function App() {
 
   return (
     <div data-theme="dark" className="">
-      {/* <HomePage /> */}
-    {/* <Router> */}
+    
       <div  className="min-h-screen bg-gray-50">
       <NavBar />
       <main>
@@ -32,17 +25,16 @@ function App() {
       <Route path="/" element={<Navigate to="/dashboard" replace/>} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/notes" element={<Notes />} />
-      {/* <Route path="/notes/:id" element={<NoteDetail />} /> */}
       <Route path="/edit/:id" element={<EditNote />} />
       <Route path="/create" element={<CreateNote />} />
       <Route path="/favorites" element={<Favorites />} />
       <Route path="/statistics" element={<Statistics />} />
 
-      {/* <Route path="/edit/:id" element={<UpdateNote />} /> */}
+   
     </Routes>
     </main>
     </div>
-    {/* </Router> */}
+  
 
     </div>
   )
